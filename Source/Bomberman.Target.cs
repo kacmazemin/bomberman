9f04c721f153ae6fad4fa4261087747f5414ab7c
emrsmsrli <emrsmsrli1@gmail.com>
2020-08-11 14:58:18 +0300

    disable unity build

--- Source/Bomberman.Target.cs
+++ Source/Bomberman.Target.cs
@@ -7,6 +7,9 @@ public class BombermanTarget : TargetRules
 {
     public BombermanTarget(TargetInfo Target) : base(Target)
     {
+        bUseUnityBuild = false;
+        bUsePCHFiles = false;
+
         Type = TargetType.Game;
         DefaultBuildSettings = BuildSettingsVersion.V2;
         ExtraModuleNames.AddRange(new string[] {"Bomberman"});
